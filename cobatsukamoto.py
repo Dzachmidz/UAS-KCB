49b505b67801f1db3910fe8bdba79b9d69f39bec
dzachmidz20 <fahmialhafidza@gmail.com>
2023-06-15 22:42:33 +0700

    fp

--- cobatsukamoto.py
+++ cobatsukamoto.py
@@ -18,13 +18,13 @@ kelembaban_normal = fuzz.trimf(kelembaban, [0, 50, 100])
 kelembaban_tinggi = fuzz.trimf(kelembaban, [50, 100, 100])
 
 # Pembentukan fungsi keanggotaan pH tanah
-ph_asam = fuzz.trimf(ph_tanah, [4, 4, 5.5])
-ph_netral = fuzz.trimf(ph_tanah, [4.5, 5.5, 6.5])
-ph_basa = fuzz.trimf(ph_tanah, [5.5, 6.5, 8])
+ph_asam = fuzz.trimf(ph_tanah, [4, 4, 6])
+ph_netral = fuzz.trimf(ph_tanah, [4, 6, 8])
+ph_basa = fuzz.trimf(ph_tanah, [6, 8, 8])
 
 # Pembentukan fungsi keanggotaan tingkat penyakit
 diagnosis_rendah = fuzz.trimf(diagnosis, [0, 0, 50])
-diagnosis_sedang = fuzz.trimf(diagnosis, [0, 50, 50])
+diagnosis_sedang = fuzz.trimf(diagnosis, [0, 50, 100])
 diagnosis_tinggi = fuzz.trimf(diagnosis, [50, 100, 100])
 
 # Definisi dataset
