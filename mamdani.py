597c754ca549549267f52e2fc2865c372552e731
dzachmidz20 <fahmialhafidza@gmail.com>
2023-06-15 11:14:59 +0700

    first commit

---
+++ mamdani.py
@@ -0,0 +1,94 @@
+import numpy as np
+import skfuzzy as fuzz
+from skfuzzy import control as ctrl
+import matplotlib.pyplot as plt
+
+# Mendefinisikan variabel linguistik
+suhu = ctrl.Antecedent(np.arange(20, 41, 1), 'suhu')
+ph_tanah = ctrl.Antecedent(np.arange(4, 9, 1), 'ph_tanah')
+kelembaban = ctrl.Antecedent(np.arange(0, 101, 1), 'kelembaban')
+diagnosis = ctrl.Consequent(np.arange(0, 101, 1), 'diagnosis')
+
+# Mendefinisikan fungsi keanggotaan untuk variabel suhu
+suhu['rendah'] = fuzz.trimf(suhu.universe, [20, 20, 30])
+suhu['normal'] = fuzz.trimf(suhu.universe, [20, 30, 40])
+suhu['tinggi'] = fuzz.trimf(suhu.universe, [30, 40, 40])
+
+# Mendefinisikan fungsi keanggotaan untuk variabel pH tanah
+ph_tanah['asam'] = fuzz.trimf(ph_tanah.universe, [4, 4, 6])
+ph_tanah['netral'] = fuzz.trimf(ph_tanah.universe, [4, 6, 8])
+ph_tanah['basah'] = fuzz.trimf(ph_tanah.universe, [6, 8, 8])
+
+# Mendefinisikan fungsi keanggotaan untuk variabel kelembaban
+kelembaban['rendah'] = fuzz.trimf(kelembaban.universe, [0, 0, 50])
+kelembaban['normal'] = fuzz.trimf(kelembaban.universe, [0, 50, 100])
+kelembaban['tinggi'] = fuzz.trimf(kelembaban.universe, [50, 100, 100])
+
+# Mendefinisikan fungsi keanggotaan untuk variabel diagnosis
+diagnosis['rendah'] = fuzz.trimf(diagnosis.universe, [0, 0, 50])
+diagnosis['sedang'] = fuzz.trimf(diagnosis.universe, [0, 50, 100])
+diagnosis['tinggi'] = fuzz.trimf(diagnosis.universe, [50, 100, 100])
+
+# Membuat aturan-aturan
+rule1 = ctrl.Rule(suhu['rendah'] & ph_tanah['asam'] & kelembaban['rendah'], diagnosis['rendah'])
+rule2 = ctrl.Rule(suhu['rendah'] & ph_tanah['asam'] & kelembaban['normal'], diagnosis['rendah'])
+rule3 = ctrl.Rule(suhu['rendah'] & ph_tanah['asam'] & kelembaban['tinggi'], diagnosis['sedang'])
+
+rule4 = ctrl.Rule(suhu['rendah'] & ph_tanah['netral'] & kelembaban['rendah'], diagnosis['rendah'])
+rule5 = ctrl.Rule(suhu['rendah'] & ph_tanah['netral'] & kelembaban['normal'], diagnosis['sedang'])
+rule6 = ctrl.Rule(suhu['rendah'] & ph_tanah['netral'] & kelembaban['tinggi'], diagnosis['tinggi'])
+
+rule7 = ctrl.Rule(suhu['rendah'] & ph_tanah['basah'] & kelembaban['rendah'], diagnosis['rendah'])
+rule8 = ctrl.Rule(suhu['rendah'] & ph_tanah['basah'] & kelembaban['normal'], diagnosis['sedang'])
+rule9 = ctrl.Rule(suhu['rendah'] & ph_tanah['basah'] & kelembaban['tinggi'], diagnosis['tinggi'])
+
+rule10 = ctrl.Rule(suhu['normal'] & ph_tanah['asam'] & kelembaban['rendah'], diagnosis['rendah'])
+rule11 = ctrl.Rule(suhu['normal'] & ph_tanah['asam'] & kelembaban['normal'], diagnosis['rendah'])
+rule12 = ctrl.Rule(suhu['normal'] & ph_tanah['asam'] & kelembaban['tinggi'], diagnosis['sedang'])
+
+rule13 = ctrl.Rule(suhu['normal'] & ph_tanah['netral'] & kelembaban['rendah'], diagnosis['rendah'])
+rule14 = ctrl.Rule(suhu['normal'] & ph_tanah['netral'] & kelembaban['normal'], diagnosis['sedang'])
+rule15 = ctrl.Rule(suhu['normal'] & ph_tanah['netral'] & kelembaban['tinggi'], diagnosis['tinggi'])
+
+rule16 = ctrl.Rule(suhu['normal'] & ph_tanah['basah'] & kelembaban['rendah'], diagnosis['rendah'])
+rule17 = ctrl.Rule(suhu['normal'] & ph_tanah['basah'] & kelembaban['normal'], diagnosis['tinggi'])
+rule18 = ctrl.Rule(suhu['normal'] & ph_tanah['basah'] & kelembaban['tinggi'], diagnosis['tinggi'])
+
+rule19 = ctrl.Rule(suhu['tinggi'] & ph_tanah['asam'] & kelembaban['rendah'], diagnosis['rendah'])
+rule20 = ctrl.Rule(suhu['tinggi'] & ph_tanah['asam'] & kelembaban['normal'], diagnosis['sedang'])
+rule21 = ctrl.Rule(suhu['tinggi'] & ph_tanah['asam'] & kelembaban['tinggi'], diagnosis['tinggi'])
+
+rule22 = ctrl.Rule(suhu['tinggi'] & ph_tanah['netral'] & kelembaban['rendah'], diagnosis['rendah'])
+rule23 = ctrl.Rule(suhu['tinggi'] & ph_tanah['netral'] & kelembaban['normal'], diagnosis['sedang'])
+rule24 = ctrl.Rule(suhu['tinggi'] & ph_tanah['netral'] & kelembaban['tinggi'], diagnosis['tinggi'])
+
+rule25 = ctrl.Rule(suhu['tinggi'] & ph_tanah['basah'] & kelembaban['rendah'], diagnosis['sedang'])
+rule26 = ctrl.Rule(suhu['tinggi'] & ph_tanah['basah'] & kelembaban['normal'], diagnosis['tinggi'])
+rule27 = ctrl.Rule(suhu['tinggi'] & ph_tanah['basah'] & kelembaban['tinggi'], diagnosis['tinggi'])
+
+# Membuat sistem fuzzy
+diagnosis_ctrl = ctrl.ControlSystem([rule1, rule2, rule3, rule4, rule5, rule6, rule7, rule8, rule9, rule10, rule11, rule12, rule13, rule14, rule15, rule16, rule17, rule18, rule19, rule20, rule21, rule22, rule23, rule24, rule25, rule26, rule27])
+diagnosa = ctrl.ControlSystemSimulation(diagnosis_ctrl)
+
+# Menerima input dari pengguna
+suhu_input = float(input("Masukkan suhu (dalam derajat Celsius): "))
+kelembaban_input = float(input("Masukkan kelembaban tanah (dalam persen): "))
+ph_input = float(input("Masukkan pH tanah: "))
+
+# Melakukan perhitungan fuzzy
+diagnosa.input['suhu'] = suhu_input
+diagnosa.input['ph_tanah'] = ph_input
+diagnosa.input['kelembaban'] = kelembaban_input
+diagnosa.compute()
+
+# Mendapatkan hasil diagnosis
+hasil_diagnosis = diagnosa.output['diagnosis']
+
+# Mencetak hasil diagnosis
+print("Hasil Diagnosis:", hasil_diagnosis)
+
+# Menampilkan kurva keanggotaan untuk variabel diagnosis
+diagnosis.view(sim=diagnosa)
+
+# Menunggu tampilan plot ditutup sebelum keluar
+plt.show()
---
+++ tsukamoto.py
@@ -0,0 +1,168 @@
+import numpy as np
+import skfuzzy as fuzz
+
+# Fuzzifikasi variabel masukan
+suhu = np.arange(20, 41, 1)
+kelembaban = np.arange(40, 101, 1)
+ph_tanah = np.arange(4, 9, 1)
+diagnosis = np.arange(0, 101, 1)
+
+# Pembentukan fungsi keanggotaan suhu
+suhu_rendah = fuzz.trimf(suhu, [20, 20, 30])
+suhu_normal = fuzz.trimf(suhu, [20, 30, 40])
+suhu_tinggi = fuzz.trimf(suhu, [30, 40, 40])
+
+# Pembentukan fungsi keanggotaan kelembaban
+kelembaban_rendah = fuzz.trimf(kelembaban, [0, 0, 50])
+kelembaban_normal = fuzz.trimf(kelembaban, [0, 50, 100])
+kelembaban_tinggi = fuzz.trimf(kelembaban, [50, 100, 100])
+
+# Pembentukan fungsi keanggotaan pH tanah
+ph_asam = fuzz.trimf(ph_tanah, [4, 4, 6])
+ph_netral = fuzz.trimf(ph_tanah, [4, 6, 8])
+ph_basa = fuzz.trimf(ph_tanah, [6, 8, 8])
+
+# Pembentukan fungsi keanggotaan tingkat penyakit
+diagnosis_rendah = fuzz.trimf(diagnosis, [0, 0, 50])
+diagnosis_sedang = fuzz.trimf(diagnosis, [0, 50, 100])
+diagnosis_tinggi = fuzz.trimf(diagnosis, [50, 100, 100])
+
+# Masukkan nilai suhu, kelembaban, dan pH tanah
+suhu_input = float(input("Masukkan nilai suhu (20-40): "))
+kelembaban_input = float(input("Masukkan nilai kelembaban (40-100): "))
+ph_tanah_input = float(input("Masukkan nilai pH tanah (4-8): "))
+
+# Proses fuzzifikasi
+suhu_rendah_degree = fuzz.interp_membership(suhu, suhu_rendah, suhu_input)
+suhu_normal_degree = fuzz.interp_membership(suhu, suhu_normal, suhu_input)
+suhu_tinggi_degree = fuzz.interp_membership(suhu, suhu_tinggi, suhu_input)
+
+kelembaban_rendah_degree = fuzz.interp_membership(kelembaban, kelembaban_rendah, kelembaban_input)
+kelembaban_normal_degree = fuzz.interp_membership(kelembaban, kelembaban_normal, kelembaban_input)
+kelembaban_tinggi_degree = fuzz.interp_membership(kelembaban, kelembaban_tinggi, kelembaban_input)
+
+ph_asam_degree = fuzz.interp_membership(ph_tanah, ph_asam, ph_tanah_input)
+ph_netral_degree = fuzz.interp_membership(ph_tanah, ph_netral, ph_tanah_input)
+ph_basa_degree = fuzz.interp_membership(ph_tanah, ph_basa, ph_tanah_input)
+
+# Inferensi dengan menggunakan metode Tsukamoto
+# Rule 1: IF suhu rendah AND kelembaban rendah AND pH asam THEN tingkat penyakit rendah
+rule1 = np.fmin(suhu_rendah_degree, np.fmin(kelembaban_rendah_degree, ph_asam_degree))
+diagnosis_output1 = np.fmin(rule1, diagnosis_rendah)
+
+# Rule 2: IF suhu normal AND kelembaban normal AND pH netral THEN tingkat penyakit rendah
+rule2 = np.fmin(suhu_normal_degree, np.fmin(kelembaban_normal_degree, ph_netral_degree))
+diagnosis_output2 = np.fmin(rule2, diagnosis_sedang)
+
+# Rule 3: IF suhu rendahsuhu_rendah AND kelembaban tinggi AND pH basa THEN tingkat penyakit tinggi
+rule3 = np.fmin(suhu_tinggi_degree, np.fmin(kelembaban_tinggi_degree, ph_basa_degree))
+diagnosis_output3 = np.fmin(rule3, diagnosis_tinggi)
+
+rule4 = np.fmin(suhu_rendah_degree, np.fmin(kelembaban_normal_degree, ph_asam_degree))
+diagnosis_output4 = np.fmin(rule4, diagnosis_rendah)
+
+rule5 = np.fmin(suhu_normal_degree, np.fmin(kelembaban_tinggi_degree, ph_netral_degree))
+diagnosis_output5 = np.fmin(rule5, diagnosis_tinggi)
+
+rule6 = np.fmin(suhu_rendah_degree, np.fmin(kelembaban_rendah_degree, ph_netral_degree))
+diagnosis_output6 = np.fmin(rule6, diagnosis_rendah)
+
+rule7 = np.fmin(suhu_rendah_degree, np.fmin(kelembaban_tinggi_degree, ph_asam_degree))
+diagnosis_output7 = np.fmin(rule7, diagnosis_sedang)
+
+rule8 = np.fmin(suhu_rendah_degree, np.fmin(kelembaban_normal_degree, ph_basa_degree))
+diagnosis_output8 = np.fmin(rule8, diagnosis_sedang)
+
+rule9 = np.fmin(suhu_rendah_degree, np.fmin(kelembaban_tinggi_degree, ph_basa_degree))
+diagnosis_output9 = np.fmin(rule9, diagnosis_tinggi)
+
+rule10 = np.fmin(suhu_normal_degree, np.fmin(kelembaban_rendah_degree, ph_asam_degree))
+diagnosis_output10 = np.fmin(rule10, diagnosis_rendah)
+
+rule11 = np.fmin(suhu_normal_degree, np.fmin(kelembaban_normal_degree, ph_asam_degree))
+diagnosis_output11 = np.fmin(rule11, diagnosis_rendah)
+
+rule12 = np.fmin(suhu_normal_degree, np.fmin(kelembaban_tinggi_degree, ph_asam_degree))
+diagnosis_output12 = np.fmin(rule12, diagnosis_sedang)
+
+rule13 = np.fmin(suhu_normal_degree, np.fmin(kelembaban_rendah_degree, ph_netral_degree))
+diagnosis_output13 = np.fmin(rule13, diagnosis_rendah)
+
+rule14 = np.fmin(suhu_normal_degree, np.fmin(kelembaban_rendah_degree, ph_basa_degree))
+diagnosis_output14 = np.fmin(rule14, diagnosis_rendah)
+
+rule15 = np.fmin(suhu_normal_degree, np.fmin(kelembaban_normal_degree, ph_basa_degree))
+diagnosis_output15 = np.fmin(rule15, diagnosis_tinggi)
+
+rule16 = np.fmin(suhu_normal_degree, np.fmin(kelembaban_tinggi_degree, ph_basa_degree))
+diagnosis_output16 = np.fmin(rule16, diagnosis_tinggi)
+
+rule17 = np.fmin(suhu_tinggi_degree, np.fmin(kelembaban_rendah_degree, ph_asam_degree))
+diagnosis_output17 = np.fmin(rule17, diagnosis_rendah)
+
+rule18 = np.fmin(suhu_tinggi_degree, np.fmin(kelembaban_normal_degree, ph_asam_degree))
+diagnosis_output18 = np.fmin(rule18, diagnosis_sedang)
+
+rule19 = np.fmin(suhu_tinggi_degree, np.fmin(kelembaban_tinggi_degree, ph_asam_degree))
+diagnosis_output19 = np.fmin(rule19, diagnosis_tinggi)
+
+rule20 = np.fmin(suhu_tinggi_degree, np.fmin(kelembaban_rendah_degree, ph_netral_degree))
+diagnosis_output20 = np.fmin(rule20, diagnosis_rendah)
+
+rule21 = np.fmin(suhu_tinggi_degree, np.fmin(kelembaban_normal_degree, ph_netral_degree))
+diagnosis_output21 = np.fmin(rule21, diagnosis_sedang)
+
+rule22 = np.fmin(suhu_tinggi_degree, np.fmin(kelembaban_tinggi_degree, ph_netral_degree))
+diagnosis_output22 = np.fmin(rule22, diagnosis_tinggi)
+
+rule23 = np.fmin(suhu_tinggi_degree, np.fmin(kelembaban_rendah_degree, ph_basa_degree))
+diagnosis_output23 = np.fmin(rule23, diagnosis_sedang)
+
+rule24 = np.fmin(suhu_tinggi_degree, np.fmin(kelembaban_normal_degree, ph_basa_degree))
+diagnosis_output24 = np.fmin(rule24, diagnosis_tinggi)
+
+rule25 = np.fmin(suhu_rendah_degree, np.fmin(kelembaban_normal_degree, ph_netral_degree))
+diagnosis_output25 = np.fmin(rule25, diagnosis_sedang)
+
+rule26 = np.fmin(suhu_rendah_degree, np.fmin(kelembaban_tinggi_degree, ph_netral_degree))
+diagnosis_output26 = np.fmin(rule26, diagnosis_tinggi)
+
+rule27 = np.fmin(suhu_rendah_degree, np.fmin(kelembaban_rendah_degree, ph_basa_degree))
+diagnosis_output27 = np.fmin(rule27, diagnosis_rendah)
+
+# Penggabungan output dari semua aturan
+diagnosis_agg = np.fmax(diagnosis_output1,
+                        np.fmax(diagnosis_output2,
+                                np.fmax(diagnosis_output3,
+                                        np.fmax(diagnosis_output4,
+                                                np.fmax(diagnosis_output5,
+                                                        np.fmax(diagnosis_output6,
+                                                                np.fmax(diagnosis_output7,
+                                                                        np.fmax(diagnosis_output8,
+                                                                                np.fmax(diagnosis_output9,
+                                                                                        np.fmax(diagnosis_output10,
+                                                                                                np.fmax(diagnosis_output11,
+                                                                                                        np.fmax(diagnosis_output12,
+                                                                                                                np.fmax(diagnosis_output13,
+                                                                                                                        np.fmax(diagnosis_output14,
+                                                                                                                                np.fmax(diagnosis_output15,
+                                                                                                                                        np.fmax(diagnosis_output16,
+                                                                                                                                                np.fmax(diagnosis_output17,
+                                                                                                                                                        np.fmax(diagnosis_output18,
+                                                                                                                                                                np.fmax(diagnosis_output19,
+                                                                                                                                                                        np.fmax(diagnosis_output20,
+                                                                                                                                                                                np.fmax(diagnosis_output21,
+                                                                                                                                                                                        np.fmax(diagnosis_output22,
+                                                                                                                                                                                                np.fmax(diagnosis_output23,
+                                                                                                                                                                                                        np.fmax(diagnosis_output24, diagnosis_output25,
+                                                                                                                                                                                                                np.fmax(diagnosis_output26, diagnosis_output27)))))))))))))))))))))))))
+                                                                                                                                                                                                        
+                                                                                                                                                                                                                        
+
+
+# Defuzzifikasi menggunakan metode Tsukamoto
+diagnosis = fuzz.defuzz(diagnosis, diagnosis_agg, 'centroid')
+diagnosis = round(diagnosis, 2)
+
+# Menampilkan hasil diagnosis
+print("Tingkat Penyakit Blast pada Padi: ", diagnosis)
